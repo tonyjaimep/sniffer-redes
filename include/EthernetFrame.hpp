0f39fe65c73c8cff0c726d6a9c64a3ed967b33ad
ttjpffs <contacto@antoniojai.me>
2021-03-05 10:14:45 -0600

    Archivos iniciales + EthernetFrame.hpp

---
+++ include/EthernetFrame.hpp
@@ -0,0 +1,26 @@
+#ifndef ETHERNETFRAME_H_INCLUDED
+#define ETHERNETFRAME_H_INCLUDED
+
+class EthernetFrame
+{
+public:
+    EthernetFrame();
+    ~EthernetFrame();
+
+    unsigned long getSourceAddress() const;
+    void setSourceAddress(unsigned long value);
+    unsigned long getDestinationAddress() const;
+    void setDestinationAddress(unsigned long value);
+    unsigned long getType() const;
+    void setType(unsigned long value);
+    char* getData() const;
+    void setData(char* value);
+
+private:
+    unsigned long sourceAddress : 6*8;
+    unsigned long destinationAddress : 6*8;
+    unsigned long type : 2*8;
+    char *data;
+};
+
+#endif // ETHERNETFRAME_H_INCLUDED
---
+++ makefile
@@ -0,0 +1,2 @@
+sniffer: src/* include/*
+	g++ -std=c++17 src/* -Iinclude -o sniffer -Wall
