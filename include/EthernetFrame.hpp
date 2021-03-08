e511261ec9479251640b6c4ba1dbed39c8d23d84
tonyjaimep <tonyjaimep@gmail.com>
2021-03-08 09:30:50 -0600

    Agregar constantes nombradas en lugar de números para longitud de campos ethernet

--- include/EthernetFrame.hpp
+++ include/EthernetFrame.hpp
@@ -1,6 +1,9 @@
 #ifndef ETHERNETFRAME_H_INCLUDED
 #define ETHERNETFRAME_H_INCLUDED
 
+#define ETHERNET_ADDRESS_LENGTH 6
+#define ETHERNET_TYPE_LENGTH 2
+
 class EthernetFrame
 {
 public:
@@ -18,10 +21,12 @@ public:
 
     void fromBytes(const char* bytes, const unsigned long& length);
 
+    static string addressAsString(const unsigned long address);
+
 private:
-    unsigned long sourceAddress : 6*8;
-    unsigned long destinationAddress : 6*8;
-    unsigned long type : 2*8;
+    unsigned long sourceAddress : ETHERNET_ADDRESS_LENGTH * 8;
+    unsigned long destinationAddress : ETHERNET_ADDRESS_LENGTH * 8;
+    unsigned long type : ETHERNET_TYPE_LENGTH * 8;
     char *data;
 };
 
